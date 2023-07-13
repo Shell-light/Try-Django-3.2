b5502ebfa0a89886903bcf1cbafd73bde7b7d435
neon1 <ulightshell@gmail.com>
2023-07-13 10:00:51 +0500

    Initialize commit

---
+++ articles/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Article
+
+class ArticelAdmin(admin.ModelAdmin):
+    list_display = ['id','title']
+    search_fields = ['title', 'content']
+
+admin.site.register(Article, ArticelAdmin)
---
+++ trydjango/views.py
@@ -0,0 +1,37 @@
+"""
+To render html web pages
+"""
+import random
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from articles.models import Article
+
+
+#MVT
+def home_view(request, *args, **kwargs):
+    """"
+    Take in a request (Django sends request)
+    Return HTML as a response (We pick to return the response)
+    """
+
+    name = "Light" # hard coded
+    random_id = random.randint(1,4) # API call to some rest API with python & python requests
+    
+    # from database ??
+    article_obj = Article.objects.get(id=random_id)
+    article_queryset = Article.objects.all()
+    context = {
+        "object_list": article_queryset,
+        "object": article_obj, 
+        "title": article_obj.title,
+        "id": article_obj.id,
+        "content": article_obj.content
+    }
+    #Django Templates
+    HTML_STRING = render_to_string("home-view.html", 
+    context=context)
+    # HTML_STRING = """
+    # <h1>{title} (id: {id})!</h1>
+    # <p>{content}</p>
+    # """.format(**context)
+    return HttpResponse(HTML_STRING)
